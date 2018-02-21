67d93c92ccbb2e547418d4f475293dec9ec9655b
Seiti Yamashiro <seiti@seiti.eti.br>
2018-02-21 12:41:13 -0300

    adding aws-shell command as well

---
+++ aws-shell/Readme.md
@@ -0,0 +1,8 @@
+AWS CLI and AWS Shell
+=====================
+
+Just build it using `build.sh`, then `sudo install.sh`.
+
+Right afterwards, `aws` and `aws-shell` commands will be available.
+
+Be happy.
--- aws-shell/install.sh
+++ aws-shell/install.sh
@@ -15,4 +15,17 @@ docker run --rm -it \\
     "\$@"
 EOF
 
+cat << EOF > /usr/local/bin/aws-shell
+#!/usr/bin/env bash
+docker run --rm  -it --entrypoint=aws-shell \\
+    -v \$(pwd):/aws \\
+    -v ~/.aws:/root/.aws \\
+    -e AWS_SECRET_ACCESS_KEY="\$AWS_SECRET_ACCESS_KEY" \\
+    -e AWS_ACCESS_KEY_ID="\$AWS_ACCESS_KEY_ID" \\
+    -e AWS_DEFAULT_REGION="\$AWS_DEFAULT_REGION" \\
+    --dns=$(nmcli dev show | grep 'DNS\[1\]' | awk '{print $NF}') \\
+    seiti/aws-cli
+EOF
+
+chmod a+x /usr/local/bin/aws-shell
 chmod a+x /usr/local/bin/aws
