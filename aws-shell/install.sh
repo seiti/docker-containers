1d0b8de9b7cfe7a17edb4696efc27382ee436647
Seiti Yamashiro <seiti@seiti.eti.br>
2018-02-21 13:59:23 -0300

    avoid showing env var o process listing (ps)

--- aws-shell/install.sh
+++ aws-shell/install.sh
@@ -7,9 +7,9 @@ cat << EOF > /usr/local/bin/aws
 #!/usr/bin/env bash
 docker run --rm -it \\
     -v \$(pwd):/aws \\
-    -e AWS_SECRET_ACCESS_KEY="\$AWS_SECRET_ACCESS_KEY" \\
-    -e AWS_ACCESS_KEY_ID="\$AWS_ACCESS_KEY_ID" \\
-    -e AWS_DEFAULT_REGION="\$AWS_DEFAULT_REGION" \\
+    -e AWS_SECRET_ACCESS_KEY \\
+    -e AWS_ACCESS_KEY_ID \\
+    -e AWS_DEFAULT_REGION \\
     --dns=$(nmcli dev show | grep 'DNS\[1\]' | awk '{print $NF}') \\
     seiti/aws-cli \\
     "\$@"
@@ -20,9 +20,9 @@ cat << EOF > /usr/local/bin/aws-shell
 docker run --rm  -it --entrypoint=aws-shell \\
     -v \$(pwd):/aws \\
     -v ~/.aws:/root/.aws \\
-    -e AWS_SECRET_ACCESS_KEY="\$AWS_SECRET_ACCESS_KEY" \\
-    -e AWS_ACCESS_KEY_ID="\$AWS_ACCESS_KEY_ID" \\
-    -e AWS_DEFAULT_REGION="\$AWS_DEFAULT_REGION" \\
+    -e AWS_SECRET_ACCESS_KEY \\
+    -e AWS_ACCESS_KEY_ID \\
+    -e AWS_DEFAULT_REGION \\
     --dns=$(nmcli dev show | grep 'DNS\[1\]' | awk '{print $NF}') \\
     seiti/aws-cli
 EOF
